8e2be5217fd71b882a3006537093bf39b4249e31
Fabian <wyssfabian1@gmail.com>
2026-07-12 21:33:19 +0200

    Part of previous

--- Assets/Particle Life/Scripts/CameraHandler.cs
+++ Assets/Particle Life/Scripts/CameraHandler.cs
@@ -30,16 +30,21 @@ public class CameraHandler : MonoBehaviour
         Vector2 Movement = MoveMouse.ReadValue<Vector2>();
         Vector2 Scroll = ScrollMouse.ReadValue<Vector2>();
         bool IsClicked = Mouse.current.leftButton.isPressed;
-        Zoom += Zoom * Scroll.y/10;
-        if(!EventSystem.current.IsPointerOverGameObject()) camera.orthographicSize = startSize * Zoom;
+        if(!EventSystem.current.IsPointerOverGameObject())
+        {
+            Zoom += Zoom * Scroll.y/10;
+            camera.orthographicSize = startSize * Zoom;
 
-        float screenHeight = camera.orthographicSize * 2;
-        float screenWidth = screenHeight * Screen.width/Screen.height;
+            float screenHeight = camera.orthographicSize * 2;
+            float screenWidth = screenHeight * Screen.width/Screen.height;
 
-        Vector3 newpos = new Vector3(Movement.x/screenWidth,Movement.y/screenHeight,0);
+            Vector3 newpos = new Vector3(Movement.x/screenWidth,Movement.y/screenHeight,0);
+            if(IsClicked) camera.transform.position -= newpos*Zoom*speed*Zoom;//(camera.orthographicSize/startSize) 
+
+        }
+
         //Vector3 newpos = new Vector3(Movement.x,Movement.y,0);
         //Debug.Log((Movement,newpos,IsClicked,Zoom,screenWidth,screenHeight));
-        if(IsClicked) camera.transform.position -= newpos*Zoom*speed*Zoom;//(camera.orthographicSize/startSize) 
 
     
     }
